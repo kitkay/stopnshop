d633139d194b5940c2bb79b728724483d42da48e
Marvin Abante <abante.marvinkaye@gmail.com>
2023-12-11 22:43:04 +0800

    Start of making cards

--- resources/js/Pages/Dashboard/DashboardProjects.vue
+++ resources/js/Pages/Dashboard/DashboardProjects.vue
@@ -26,15 +26,14 @@ defineProps({
 </script>
 
 <template>
-        <div class="flex flex-col text-sm font-medium text-gray-500 truncate">
-            <span class="flex h-full items-center ">Product
-                <span class="font-extrabold bg-black text-white px-3 mx-1 rounded">[{{ id }} - {{ sku }}]</span>
-                {{ productname }}
-            </span>
-            <span class="uppercase">Catgeory: {{ category }}</span>
-        </div>
-        <div class="mt-1 text-2xl font-semibold text-gray-900">
-            {{ desp }}
-        </div>
-
+    <div class="flex flex-col text-sm font-medium text-gray-500 truncate">
+        <span class="flex h-full items-center ">Product
+            <span class="font-extrabold bg-black text-white px-3 mx-1 rounded">[{{ id }} - {{ sku }}]</span>
+            {{ productname }}
+        </span>
+        <span class="uppercase">Catgeory: {{ category }}</span>
+    </div>
+    <div class="mt-1 text-2xl font-semibold text-gray-900">
+        {{ desp }}
+    </div>
 </template>
